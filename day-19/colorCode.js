b30e97dda001f60a6a46eddd97964e0c2edfab71
Snehal Narute <snehaln5551997@gmail.com>
2021-11-29 23:52:49 +0530

    day19

---
+++ day-19/colorCode.js
@@ -0,0 +1,17 @@
+const colorCode = (color) => {
+	return colors.indexOf(color)
+}
+const colors = [
+	'black',
+	'brown',
+	'red',
+	'orange',
+	'yellow',
+	'green',
+	'blue',
+	'violet',
+	'grey',
+	'white',
+]
+console.log(colorCode('blue'))
+console.log(colorCode('white'))
